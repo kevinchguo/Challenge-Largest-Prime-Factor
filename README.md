6ea59398f35a91878cf12674ce75d566952b14d5
Kevin Guo <Kevin@Kevins-Air.home>
2019-07-13 13:41:10 -1000

    work on putting new factors in array

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Largest Prime Factor
 
 ## Challenge 3
+
 The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 
 ## Getting Started
+
 1. Fork this repository and clone it from your personal GitHub Account
 1. In the Terminal, navigate to the newly created folder for this repository
 1. Install dependencies by running the command: `npm install`
--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,32 @@
 exports.largestPrimeFactor = function(n) {
   let primeNumber = 0;
-  // do your work here
+  let factors = [];
+  // let primeNumber = function(n) {
+  //   if ((factors[0] = 1)) {
+  //     primeNumber = factors[0];
+  //     return;
+  //   }
+  for (let i = 0; i < n; i++) {
+    if (n % i === 0) {
+      factors.unshift(i);
+    }
+  }
+  console.log(factors);
+  let arry2 = [];
+  // let findPrime = function(arry) {
+  let factors2 = [];
 
+  for (let x = 0; x < n; x++) {
+    if (factors[x] % x === 0) {
+      factors2.unshift(x);
+    }
+    console.log(factors2);
+    // if (factors2.length === 1) {
+    //   primeNumber = factors[x];
+    // }
+  }
+  // };
+  // primeNumber(array[0]);
+  // };
   return primeNumber;
 };
