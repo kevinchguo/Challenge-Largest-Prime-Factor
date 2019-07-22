e245f4204a5341f25c7d92f5acc190ae6c869ead
Kevin Guo <Kevin@Kevins-Air.home>
2019-07-21 17:56:25 -1000

    finished

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,32 +1,15 @@
 exports.largestPrimeFactor = function(n) {
   let primeNumber = 0;
-  let factors = [];
-  // let primeNumber = function(n) {
-  //   if ((factors[0] = 1)) {
-  //     primeNumber = factors[0];
-  //     return;
-  //   }
-  for (let i = 0; i < n; i++) {
-    if (n % i === 0) {
-      factors.unshift(i);
+  // n will become and stay as the largest prime factor until the while loop ends when x becomes greater than n
+  for (let x = 3; x < n; ) {
+    if (n % x === 0) {
+      n /= x;
+      //x will only update IF n % x !== 0
+    } else {
+      x += 2;
     }
   }
-  console.log(factors);
-  let arry2 = [];
-  // let findPrime = function(arry) {
-  let factors2 = [];
 
-  for (let x = 0; x < n; x++) {
-    if (factors[x] % x === 0) {
-      factors2.unshift(x);
-    }
-    console.log(factors2);
-    // if (factors2.length === 1) {
-    //   primeNumber = factors[x];
-    // }
-  }
-  // };
-  // primeNumber(array[0]);
-  // };
+  primeNumber = n;
   return primeNumber;
 };
